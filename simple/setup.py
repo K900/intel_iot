5643c02875deeddb2df8984947ce21d71dc3a5df
Artem <art1415926535@users.noreply.github.com>
2015-06-12 11:40:32 +0300

    Fix

--- simple/setup.py
+++ simple/setup.py
@@ -3,6 +3,6 @@ from setuptools import setup
 setup(
     name='intel_iot-simple',
     version='0.1.0',
-    py_modules=['blink', 'button', 'joystick', 'light', 'noise''],
+    py_modules=['blink', 'button', 'joystick', 'light', 'noise'],
     license='?',
 )
